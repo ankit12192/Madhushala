e6373153c7e7eaa0b0915299420de72b429425ee
Ankit Tiwari <ankittiwari@Ankits-MacBook-Air.local>
2020-05-03 09:18:24 +0530

    Changes in Theme

--- js/custom.js
+++ js/custom.js
@@ -1,139 +1,99 @@
-jQuery(document).ready(function($) {
-
-	$('.scrollup').click(function(){
-		$("html, body").animate({ scrollTop: 0 }, 1000);
-		return false;
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
 	});
 	
-		$('.accordion').on('show', function (e) {
-		
-			$(e.target).prev('.accordion-heading').find('.accordion-toggle').addClass('active');
-			$(e.target).prev('.accordion-heading').find('.accordion-toggle i').removeClass('icon-plus');
-			$(e.target).prev('.accordion-heading').find('.accordion-toggle i').addClass('icon-minus');
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
+    
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+	
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	$('#slides').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+	
+	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
+	
+	/* ..............................................
+    Map Full
+    ................................................. */
+	
+	$(document).ready(function(){ 
+		$(window).on('scroll', function () {
+			if ($(this).scrollTop() > 100) { 
+				$('#back-to-top').fadeIn(); 
+			} else { 
+				$('#back-to-top').fadeOut(); 
+			} 
+		}); 
+		$('#back-to-top').click(function(){ 
+			$("html, body").animate({ scrollTop: 0 }, 600); 
+			return false; 
+		}); 
+	});
+	
+	/* ..............................................
+    Special Menu
+    ................................................. */
+	
+	var Container = $('.container');
+	Container.imagesLoaded(function () {
+		var portfolio = $('.special-menu');
+		portfolio.on('click', 'button', function () {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
 		});
-		
-		$('.accordion').on('hide', function (e) {
-			$(this).find('.accordion-toggle').not($(e.target)).removeClass('active');
-			$(this).find('.accordion-toggle i').not($(e.target)).removeClass('icon-minus');
-			$(this).find('.accordion-toggle i').not($(e.target)).addClass('icon-plus');
-		});	
-
-	$('.navigation').onePageNav({
-		begin: function() {
-			console.log('start');
-		},
-		end: function() {
-			console.log('stop');
-		},
-			scrollOffset: 0		
+		var $grid = $('.special-list').isotope({
+			itemSelector: '.special-grid'
+		});
 	});
 	
-	// prettyPhoto
-	$("a[data-pretty^='prettyPhoto']").prettyPhoto();		
-
-    // Localscrolling 
-	$('#menu-main, .brand').localScroll();
+	/* ..............................................
+    BaguetteBox
+    ................................................. */
 	
-	$('#menu-main li a').click(function(){
-		var links = $('#menu-main li a');
-		links.removeClass('selected');
-		$(this).addClass('selected');
+	baguetteBox.run('.tz-gallery', {
+		animation: 'fadeIn',
+		noScrollbars: true
 	});
-
-    var iOS = false,
-        p = navigator.platform;
-
-    if (p === 'iPad' || p === 'iPhone' || p === 'iPod') {
-        iOS = true;
-    }	
-	
-    if (iOS === false) {
-
-        $('.flyIn').bind('inview', function (event, visible) {
-            if (visible === true) {
-                $(this).addClass('animated fadeInUp');
-            }
-        });
-
-        $('.flyLeft').bind('inview', function (event, visible) {
-            if (visible === true) {
-                $(this).addClass('animated fadeInLeftBig');
-            }
-        });
-
-        $('.flyRight').bind('inview', function (event, visible) {
-            if (visible === true) {
-                $(this).addClass('animated fadeInRightBig');
-            }
-        });
-
-    }
-	
-	// add animation on hover
-		$(".service-box").hover(
-			function () {
-			$(this).find('img').addClass("animated pulse");
-			$(this).find('h2').addClass("animated fadeInUp");
-			},
-			function () {
-			$(this).find('img').removeClass("animated pulse");
-			$(this).find('h2').removeClass("animated fadeInUp");
-			}
-		);
-		
-	
-	// cache container
-	var $container = $('#portfolio-wrap');
-	$.browser.safari = ($.browser.webkit && !(/chrome/.test(navigator.userAgent.toLowerCase())));	
-	
-	if($.browser.safari){ 	
-	// initialize isotope
-	$container.isotope({
-		animationEngine : 'jquery',
-		animationOptions: {
-			duration: 200,
-			queue: false
-		},
-		layoutMode: 'fitRows'
-	});
-	} else {	
-	$container.isotope({
-		animationEngine : 'best-available',
-		animationOptions: {
-			duration: 200,
-			queue: false
-		},
-		layoutMode: 'fitRows'
-	});	
-	
-	$(window).resize(function() {
-		$container.isotope('reLayout');
-	});
-	}
-	// filter items when filter link is clicked
-	$('#filters a').click(function(){
-		$('#filters a').removeClass('active');
-		$(this).addClass('active');
-		var selector = $(this).attr('data-filter');
-		$container.isotope({ filter: selector });
-		return false;
-	});
-
-	// flexslider main
-	$('#main-flexslider').flexslider({						
-		animation: "swing",
-		direction: "vertical", 
-		slideshow: true,
-		slideshowSpeed: 3500,
-		animationDuration: 1000,
-		directionNav: true,
-		prevText: '<i class="icon-angle-up icon-2x"></i>',       
-		nextText: '<i class="icon-angle-down icon-2x active"></i>', 
-		controlNav: false,
-		smootheHeight:true,						
-		useCSS: false
-	});
-});
 	
 	
-
+	
+	/* ..............................................
+    Datepicker
+    ................................................. */
+	
+	$('.datepicker').pickadate();
+	
+	$('.time').pickatime();
+	
+	
+	
+	
+	
+}(jQuery));
